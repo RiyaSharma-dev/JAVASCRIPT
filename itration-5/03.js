581ffffc925ff96afd58a538ef4a744b39d70c59
Riya <riyasharma.fsd121@gmail.com>
2025-12-04 18:13:42 +0530

    Initial commit

---
+++ itration-5/03.js
@@ -0,0 +1,17 @@
+// for of loop
+
+// ["","",""]
+//[{},{}, {}]
+
+const arr = [1, 2,3,4,5]
+
+for(const iterator of arr){
+console.log(iterator);
+
+}
+
+const string = "riya"
+for(const num of string){
+    console.log(num);
+    
+}git
